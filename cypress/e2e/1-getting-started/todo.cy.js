a801005a0219886e94337d6b13bb21ec7996eb4e
Your Name <you@example.com>
2023-11-21 16:47:00 +0530

    sample changes

--- cypress/e2e/1-getting-started/todo.cy.js
+++ cypress/e2e/1-getting-started/todo.cy.js
@@ -12,6 +12,7 @@
 // https://on.cypress.io/introduction-to-cypress
 
 describe('example to-do app', () => {
+  // new comment
   beforeEach(() => {
     // Cypress starts out with a blank slate for each test
     // so we must tell it to visit our website with the `cy.visit()` command.
